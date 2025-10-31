7b9a144ccfdd79320a7c06e604eefbc6f3b8af60
MaestroChili <112980380+MaestroChili@users.noreply.github.com>
2025-10-31 15:25:43 -0400

    Add function to plot best fit line

--- scripts/plot_script.js
+++ scripts/plot_script.js
@@ -9,6 +9,21 @@ function covariance(x, y, meanX, meanY) {
 	return accumulator / x.length;
 }
 
+function plotBestFitLine(x, y, divName, name) {
+	let meanOfX = mean(x);
+	let meanOfY = mean(y);
+	let slope = covariance(x, y, meanOfX, meanOfY) / variance(x, meanOfX);
+	let yInt = meanOfY - slope * meanOfX;
+
+	const bestFitLine = [{
+		x: [0, 90],
+		y: [m*0 + b, m*90 + b],
+		type: 'scatter',
+		name: name
+	}];
+	Plotly.plot(divName, bestFitLine);
+}
+
 let xdata = [1, 2, 3, 3, 4, 6, 5, 7, 8, 8, 9, 10, 10, 12, 11, 14, 13, 16, 14, 15,
 	17, 16, 18, 20, 19, 19, 21, 22, 24, 23, 24, 25, 27, 28, 29, 30, 33, 33, 32, 31,
 	34, 36, 37, 36, 38, 37, 39, 44, 41, 42, 42, 43, 44, 46, 45, 47, 48, 47, 49, 50,
@@ -23,10 +38,10 @@ let ydata = [2, 1, 3, 4, 5, 5, 8, 6, 9, 10,10, 12, 11, 14, 13, 15, 14, 17, 16, 1
 
 console.log(xdata.length, ydata.length);
 
-let meanX = mean(xdata);
-let meanY = mean(ydata);
-let m = covariance(xdata, ydata, meanX, meanY) / variance(xdata, meanX);
-let b = meanY - m * meanX;
+// let meanX = mean(xdata);
+// let meanY = mean(ydata);
+// let m = covariance(xdata, ydata, meanX, meanY) / variance(xdata, meanX);
+// let b = meanY - m * meanX;
 
 const data = [{
 	x: xdata,
@@ -37,19 +52,19 @@ const data = [{
 	name: 'male'
 }];
 
-console.log(meanX);
-console.log(meanY);
-console.log("Covariance: " + covariance(xdata, ydata, meanX, meanY));
-console.log("Slope " + m);
-console.log(b);
+// console.log(meanX);
+// console.log(meanY);
+// console.log("Covariance: " + covariance(xdata, ydata, meanX, meanY));
+// console.log("Slope " + m);
+// console.log(b);
 
-const moreData = [{
-	x: [0, 90],
-	y: [m*0 + b, m*90 + b],
-	type: 'scatter',
-	name: "Best Fit Line"
-}];
+// const bestFitLine = [{
+// 	x: [0, 90],
+// 	y: [m*0 + b, m*90 + b],
+// 	type: 'scatter',
+// 	name: "Best Fit Line"
+// }];
 
 Plotly.newPlot('plot-tester', data);
-Plotly.plot('plot-tester', moreData);
+plotBestFitLine(xdata, ydata, 'plot-tester', 'best fit line');
 
