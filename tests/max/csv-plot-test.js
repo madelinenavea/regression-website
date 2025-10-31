1a12c617f575413cb53287a358becd04b969c74d
MaestroChili <112980380+MaestroChili@users.noreply.github.com>
2025-10-31 13:54:20 -0400

    Improve console log format in csv-plot-test.js
    Refactor console log to improve readability

--- tests/max/csv-plot-test.js
+++ tests/max/csv-plot-test.js
@@ -7,7 +7,8 @@ Plotly.d3.csv("../../data/Salary_dataset.csv", function(err, rows) {
   }
 
   const columns = Object.keys(rows[0]);
-  console.log("Columns:", columns);
+  console.log("Columns:");
+  console.log(columns);
 
   columns.forEach(col => {
     const option = document.createElement("option");
